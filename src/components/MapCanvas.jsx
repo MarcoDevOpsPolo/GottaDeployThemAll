c87bc5819312bf55584e5ce043baf167c9131ccf
majorosgereby <25921022+majorosgereby@users.noreply.github.com>
2024-06-10 14:21:50 +0200

    add MapCanvas main logic

--- src/components/MapCanvas.jsx
+++ src/components/MapCanvas.jsx
@@ -1,1 +1,42 @@
-/**/
+/* a canvas to travel between locations on the map */
+import React, { useRef, useEffect, useState } from 'react'
+
+export default function MapCanvas(props) {
+
+    //refs
+    const canvasRef = useRef(null)
+
+    //states
+    const [playerState, setPlayerState] = useState({})
+
+    //local functions
+    const draw = (context) => {
+
+    }
+
+    window.onkeydown = (e) => {
+
+    }
+
+    //use effects
+    useEffect(() => {
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+        let animationFrameId
+
+        const render = () => {
+            draw(context)
+            animationFrameId = window.requestAnimationFrame(render)
+        }
+        render()
+
+        return () => {
+            window.cancelAnimationFrame(animationFrameId)
+        }
+
+    }, [draw])
+
+    return (
+        <canvas ref={canvasRef} className='canvas-main'></canvas>
+    )
+}
