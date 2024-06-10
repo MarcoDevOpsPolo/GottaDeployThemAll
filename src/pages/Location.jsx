c8a43dd88c519553633a9bfb45f0801fccbc986f
Stefanov Emilia <stemilia92@gmail.com>
2024-06-10 17:30:39 +0300

    randomize pokemon encounter

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -1,17 +1,18 @@
 /* encounter at location with pokemon */
 
 import { useEffect, useState } from "react"
+import calculatePokemonEncounter from "../services/multiFuncty"
 
 function Location(props) {
     const [pokemons, setPokemons] = useState([])
-    const [encounter, setEncounter] = useState(null)
+    const [encounter, setEncounter] = useState({encount: false, pokemon: null})
 
     useEffect(() => {
         //Mount
         async function fetchPokemons() {
             try {
                 // url from props.url
-                const response = await fetch("https://pokeapi.co/api/v2/location-area/1/")
+                const response = await fetch("https://pokeapi.co/api/v2/location-area/2/")
                 const jsonData = await response.json()
                 const pokemons = jsonData["pokemon_encounters"]
                 setPokemons(pokemons)
@@ -29,10 +30,45 @@ function Location(props) {
 
     useEffect(() => {
         if (pokemons.length > 0) {
+            let pokemon;
+            let options;
+            const chances = pokemons.map(poke => poke["version_details"][0]["max_chance"])
+            const noEncounterChance = pokemons.reduce( (chance, poke) => poke["version_details"][0]["max_chance"] < chance ? poke["version_details"][0]["max_chance"] : chance, Math.max(...chances)) + 10
+            let randomNumber = Math.round(Math.random() * Math.max(...chances))
+
             console.log("pokemons: ", pokemons)
+            console.log("chances:", chances)
+            console.log("randomNumber:", randomNumber)
+            console.log("no enc: " + noEncounterChance)
             
+            if (randomNumber <= noEncounterChance) {
+                const rareEncounter = chances.find(chance => chance === randomNumber)
+                if (rareEncounter > 0) {
+                    options = pokemons.filter(poke => poke["version_details"][0]["max_chance"] === randomNumber)
+                    pokemon = calculatePokemonEncounter(options)
+                } else {
+                    console.log("No encounter!")
+                    return
+                }
+            }
+
+            if (chances.includes(randomNumber)) {
+                options = pokemons.filter(poke => poke["version_details"][0]["max_chance"] === randomNumber)
+                pokemon = calculatePokemonEncounter(options)
+            } else {
+                options = pokemons.filter(poke => poke["version_details"][0]["max_chance"] >= randomNumber)
+                pokemon = calculatePokemonEncounter(options)
+            }
+            
+            setEncounter({...encounter, encount: true, pokemon: pokemon})
         }
     }, [pokemons])
+
+    useEffect(() => {
+        if (encounter.encount) {
+            console.log("Encounter with this pokemon: ", encounter.pokemon)
+        }
+    }, [encounter])
 
     return (
         <>
--- src/services/multiFuncty.js
+++ src/services/multiFuncty.js
@@ -1,1 +1,7 @@
 /* for all time multi use functions */
+
+export default function calculatePokemonEncounter(options) {
+    const randomNumber = Math.floor(Math.random() * options.length);
+    const pokemon = options[randomNumber];
+    return pokemon
+}
