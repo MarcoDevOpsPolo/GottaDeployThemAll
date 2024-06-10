2a3b3d6deb11779d7b842fc4dcd25bec647ea8af
majorosgereby <25921022+majorosgereby@users.noreply.github.com>
2024-06-10 15:12:21 +0200

    introduce new unit canvasPixel (x,y) for scalebal canvas movements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import MapCanvas from './components/MapCanvas'
 
 function App() {
 
   return (
     <>
-
+      <MapCanvas></MapCanvas>
     </>
   )
 }
--- src/components/MapCanvas.jsx
+++ src/components/MapCanvas.jsx
@@ -1,5 +1,6 @@
 /* a canvas to travel between locations on the map */
 import React, { useRef, useEffect, useState } from 'react'
+import background3Src from './../assets/maps/map3.png'
 
 export default function MapCanvas(props) {
 
@@ -8,10 +9,26 @@ export default function MapCanvas(props) {
 
     //states
     const [playerState, setPlayerState] = useState({})
+    const [canvasPixel, setCanvasPixel] = useState({
+        x: 1,
+        y: 1,
+        isSet: false
+    })
+
+    //imgs
+    const backgroundImg = new Image()
+    backgroundImg.src = background3Src
+
+    //local variables
+
 
     //local functions
+    /**
+     * @param {CanvasRenderingContext2D} context 
+     */
     const draw = (context) => {
-
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height)
+        context.drawImage(backgroundImg, 0, 0, context.canvas.width, context.canvas.height)
     }
 
     window.onkeydown = (e) => {
@@ -22,6 +39,16 @@ export default function MapCanvas(props) {
     useEffect(() => {
         const canvas = canvasRef.current
         const context = canvas.getContext('2d')
+        context.canvas.width = window.innerWidth * 0.987
+        context.canvas.height = window.innerHeight * 0.98
+        if (!canvasPixel.isSet) {
+            setCanvasPixel({
+                x: context.canvas.width / backgroundImg.width,
+                y: context.canvas.height / backgroundImg.height,
+                isSet: true
+            })
+        }
+
         let animationFrameId
 
         const render = () => {
@@ -35,6 +62,10 @@ export default function MapCanvas(props) {
         }
 
     }, [draw])
+
+    useEffect(() => {
+        console.log(canvasPixel)
+    }, [canvasPixel])
 
     return (
         <canvas ref={canvasRef} className='canvas-main'></canvas>
