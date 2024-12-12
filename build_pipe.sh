c741a433b18554774c6e9fe13b4d306ae33a3585
majorosgereby <25921022+majorosgereby@users.noreply.github.com>
2024-12-12 12:06:41 +0100

    add building of docker image and running container from buildpipe

--- build_pipe.sh
+++ build_pipe.sh
@@ -7,3 +7,16 @@ cd frontend || { echo "Failed to enter 'frontend' directory"; exit 1; }
 npm run build
 
 cd ..
+
+cd backend || { echo "Failed to enter 'backend' directory"; exit 1; }
+
+docker rmi poke-api
+
+docker build -t poke-api .
+
+cd ..
+
+CONTAINER_ID=$(docker run -d -p 8128:8128 poke-api )
+
+echo "id: $CONTAINER_ID"
+
