b9021fcdb4452880607e9f1d7e56cb610ace79e3
majorosgereby <25921022+majorosgereby@users.noreply.github.com>
2024-12-12 15:00:58 +0100

    add building of analyser image

--- build_pipe.sh
+++ build_pipe.sh
@@ -1,19 +1,35 @@
-#!/bin/bash
+#!//bash
 #Dont forget to run this script as a source! '. ./build_pipe.sh'
 
-if [[ -n "$CONTAINER_ID" ]] 
+# Kill Poke Container
+
+if [[ -n "$POKE_CONTAINER_ID" ]] 
 then
-    docker kill "$CONTAINER_ID"
-    docker rm "$CONTAINER_ID"
+    docker kill "$POKE_CONTAINER_ID"
+    docker rm "$POKE_CONTAINER_ID"
 fi
 
 echo "gotta catch em all"
+
+# Analyser container build
+
+cd analysis || { echo "Failed to enter 'analysis' directory"; exit 1; }
+
+docker rmi -f analyser
+
+docker build -t analyser
+
+cd ..
+
+# Frontend React Build
 
 cd frontend || { echo "Failed to enter 'frontend' directory"; exit 1; }
 
 npm run build
 
 cd ..
+
+# Backend docker container creation
 
 cd backend || { echo "Failed to enter 'backend' directory"; exit 1; }
 
@@ -23,9 +39,9 @@ docker build -t poke-api .
 
 cd ..
 
-CONTAINER_ID=$(docker run -d -p 8128:8128 poke-api )
-export CONTAINER_ID
+POKE_CONTAINER_ID=$(docker run -d -p 8128:8128 poke-api )
+export POKE_CONTAINER_ID
 
-echo "id: $CONTAINER_ID"
+echo "id: $POKE_CONTAINER_ID"
 
-docker logs -f "$CONTAINER_ID" >> ./analysis/log.txt 2>&1 &
+docker logs -f "$POKE_CONTAINER_ID" >> ./analysis/log.txt 2>&1 &
