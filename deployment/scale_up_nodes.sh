72a5eab39c73c9f105c8f4321e13f2014b8aba51
Stefanov Emilia <stemilia92@gmail.com>
2025-01-08 16:37:13 +0300

    add region to node scaling file

--- deployment/scale_up_nodes.sh
+++ deployment/scale_up_nodes.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-eksctl scale nodegroup --name=linux-nodes --cluster=poke-cluster --nodes=30 --nodes-min=1 --nodes-max=30
+eksctl scale nodegroup --region eu-west-2 --name=linux-nodes --cluster=poke-cluster --nodes=15 --nodes-min=1 --nodes-max=15
