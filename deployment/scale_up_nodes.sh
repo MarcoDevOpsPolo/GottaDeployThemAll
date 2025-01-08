e9a281aec852c7f9b39b16659d19f3126c099e73
Stefanov Emilia <stemilia92@gmail.com>
2025-01-08 17:18:20 +0300

    scale nodes to 10

--- deployment/scale_up_nodes.sh
+++ deployment/scale_up_nodes.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-eksctl scale nodegroup --region eu-west-2 --name=linux-nodes --cluster=poke-cluster --nodes=15 --nodes-min=1 --nodes-max=15
+eksctl scale nodegroup --region eu-west-2 --name=linux-nodes --cluster=poke-cluster --nodes=10 --nodes-min=1 --nodes-max=10
