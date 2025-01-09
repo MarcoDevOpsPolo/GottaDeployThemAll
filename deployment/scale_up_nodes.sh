bbcaefbba9744cf53a9e31dbc145affbfb03f6d6
Stefanov Emilia <stemilia92@gmail.com>
2025-01-09 11:58:57 +0300

    scale up nodes and install kube-prom-stack (with grafana)

--- deployment/scale_up_nodes.sh
+++ deployment/scale_up_nodes.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-eksctl scale nodegroup --region eu-west-2 --name=linux-nodes --cluster=poke-cluster --nodes=10 --nodes-min=1 --nodes-max=10
+eksctl scale nodegroup --region eu-west-2 --name=linux-nodes --cluster=poke-cluster --nodes=35 --nodes-min=1 --nodes-max=35
