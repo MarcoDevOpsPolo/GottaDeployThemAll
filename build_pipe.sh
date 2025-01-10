455c94a520a0d2254d1dd9ab01489111f2f7f2b8
Stefanov Emilia <stemilia92@gmail.com>
2025-01-10 14:00:22 +0300

    set build_pipe to handle analysis as well

--- build_pipe.sh
+++ build_pipe.sh
@@ -2,6 +2,16 @@
 
 echo "gotta catch em all"
 
+# Create log folder
+
+if [ -d "logs" ]; then
+	  rm -rf logs
+fi
+
+mkdir logs
+
+touch logs/log.txt
+
 # Frontend React Build
 
 cd frontend || { echo "Failed to enter 'frontend' directory"; exit 1; }
@@ -11,7 +21,7 @@ npm run build
 cd ..
 
 
-# Start docker compose
+# Start docker gottafetchthemall
 
 cd backend
 
@@ -19,5 +29,12 @@ docker build -t gottafetchtemall .
 
 cd ..
 
+# Start docker analysis
+
+cd analysis
+
+docker build -t analyser .
+
+cd ..
 # here lies the tomb of approximately 20 deleted lines. Let this be a memoir on hard working o7
 
